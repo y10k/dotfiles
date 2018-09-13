a61718804356ef0e0c68f1bf6b29b26c4200fc47
TOKI Yoshinori <toki@freedom.ne.jp>
2018-09-13 20:48:53 +0900

    manual setup script for ubuntu on wsl

---
+++ start_wsl.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+sudo service rsyslog start
+sudo service ssh start
+sudo service screen-cleanup start
+
+# not used Ubuntu 18.04 on Windows Subsystem for Linux
+#sudo sudo cgroupfs-mount
+#sudo service docker start
