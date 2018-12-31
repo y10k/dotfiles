0bd1369e740fe22782f5b4c02765ff1fecd602c1
TOKI Yoshinori <toki@freedom.ne.jp>
2018-12-31 17:39:45 +0900

    modify ruby-unit-test-run-test-method: `test_file.rb -ntest_method' -> `test_file.rb -t/TestClass/ -ntest_method'

--- elisp/work/ruby-test-unit.el
+++ elisp/work/ruby-test-unit.el
@@ -26,18 +26,31 @@
    (ruby-unit-test-get-point-at-beginning-of-line)
    (ruby-unit-test-get-point-at-end-of-line)))
 
+(defun ruby-unit-test-goto-test-class-definition ()
+  "クラス定義の行へ移動する。"
+  (end-of-line)                         ;カレント行を検索対象に含めるため
+  (let ((case-fold-search nil))
+    (re-search-backward "\\(^\\|\\s-\\)class\\s-+[A-Z][A-Za-z0-9_]+\\s-*<\\s-*Test::Unit::TestCase" nil t)))
+
 (defun ruby-unit-test-get-test-method-name (line)
   "Ruby Test::Unitのテストメソッドの名前を文字列から取り出して返す。"
   (let ((case-fold-search nil))
     (if (string-match "\\(^\\|\\s-\\)def\\s-+\\(test_[A-Za-z0-9_]+\\??\\)" line)
         (match-string 2 line))))
 
-(defun ruby-unit-test-get-test-method-command-string (test-file-name test-method-name &optional test-options ruby-options)
-  "Ruby Test::Unitを実行するコマンドを文字列で返す。"
+(defun ruby-unit-test-get-test-class-name (line)
+  "Ruby Test::Unitのテストクラスの名前を文字列から取り出して返す。"
+  (let ((case-fold-search nil))
+    (if (string-match "\\(^\\|\\s-\\)class\\s-+\\([A-Z][A-Za-z0-9_]+\\)\\s-*<\\s-*Test::Unit::TestCase" line)
+        (match-string 2 line))))
+
+(defun ruby-unit-test-get-test-method-command-string (test-file-name test-class-name test-method-name &optional test-options ruby-options)
+  "Ruby Test::Unitのテストメソッドを実行するコマンドを文字列で返す。"
   (concat "bundle exec ruby "
           (if ruby-options (concat ruby-options " ") "")
           test-file-name
           (if test-options (concat " " test-options) "")
+          " -t/" test-class-name "/"
           " -n" test-method-name))
 
 (defun ruby-unit-test-run-test-method (ruby-debug-option-p)
@@ -47,11 +60,21 @@
     (let ((test-file-name (ruby-unit-test-get-test-file-name))
           (test-method-name (ruby-unit-test-get-test-method-name (ruby-unit-test-get-line))))
       (if (and test-file-name test-method-name)
-          (let ((command-string
-                 (if ruby-debug-option-p
-                     (ruby-unit-test-get-test-method-command-string test-file-name test-method-name ruby-unit-test-runner-options "-d")
-                   (ruby-unit-test-get-test-method-command-string test-file-name test-method-name ruby-unit-test-runner-options))))
-            (compile command-string))
+          (if (ruby-unit-test-goto-test-class-definition)
+              (let ((test-class-name (ruby-unit-test-get-test-class-name (ruby-unit-test-get-line))))
+                (let ((command-string
+                       (if ruby-debug-option-p
+                           (ruby-unit-test-get-test-method-command-string test-file-name
+                                                                          test-class-name
+                                                                          test-method-name
+                                                                          ruby-unit-test-runner-options
+                                                                          "-d")
+                         (ruby-unit-test-get-test-method-command-string test-file-name
+                                                                        test-class-name
+                                                                        test-method-name
+                                                                        ruby-unit-test-runner-options))))
+                  (compile command-string)))
+            (message "Not found a Ruby Test::Unit test-case class."))
         (message "Not found a Ruby Test::Unit method here.")))))
 
 ; Local Variables:
